ad78f4aea8229183fad00d35922ce256f60050b0
AManzouri <49921870+AManzouri@users.noreply.github.com>
2019-04-27 11:51:56 +0430

    Update __init__.py
    change dataset address

--- mnist/__init__.py
+++ mnist/__init__.py
@@ -10,7 +10,7 @@ import sys
 
 #from: https://github.com/datapythonista/mnist
 DATASET_DIRECTORY = 'data/'
-URL = 'http://yann.lecun.com/exdb/mnist/'
+URL = 'http://farsiocr.ir/%D9%85%D8%AC%D9%85%D9%88%D8%B9%D9%87-%D8%AF%D8%A7%D8%AF%D9%87/%D9%85%D8%AC%D9%85%D9%88%D8%B9%D9%87-%D8%A7%D8%B1%D9%82%D8%A7%D9%85-%D8%AF%D8%B3%D8%AA%D9%86%D9%88%DB%8C%D8%B3-%D9%87%D8%AF%DB%8C/'
 
 def parse_idx(fd):
     DATA_TYPES = {0x08: 'B',  # unsigned byte
@@ -73,7 +73,7 @@ def download_and_parse_mnist_file(fname, target_dir=None, force=False):
 
 
 def train_images():
-    return download_and_parse_mnist_file('train-images-idx3-ubyte.gz')
+    return download_and_parse_mnist_file('Digit Dataset.zip')
 
 
 def test_images():
